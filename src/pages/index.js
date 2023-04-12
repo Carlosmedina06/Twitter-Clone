2f36dad5df3018778eb4d2d0bcf1598da55d3817
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-12 02:16:07 -0300

    feed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import Feed from '@/components/Feed/Feed'
 import Sidebar from '@/components/Sidebar/Sidebar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen max-w-[1500px] mx-auto">
       <Sidebar />
+      <Feed />
     </main>
   )
 }
