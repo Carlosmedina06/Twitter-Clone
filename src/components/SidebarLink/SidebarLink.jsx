a7f1fdfd59217fab6f0bdd84b0654cbaac47ab19
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-12 02:15:31 -0300

    sidebarlink color

--- src/components/SidebarLink/SidebarLink.jsx
+++ src/components/SidebarLink/SidebarLink.jsx
@@ -2,7 +2,7 @@ const SidebarLink = ({ title, Icon, active }) => {
   return (
     <div
       className={`flex items-center justify-center xl:justify-start text-[#d9d9d9] text-xl space-x-3
-      ${active && 'font-bold text-blue-400'}
+      ${active && 'font-bold'}
       hover:bg-[#d9d9d9] hover:bg-opacity-10 rounded-full cursor-pointer w-[52px] h-[52px] xl:w-auto xl:h-auto xl:py-3 xl:px-4 transition duration-200 ease-out`}
     >
       <Icon className="text-2xl" />
