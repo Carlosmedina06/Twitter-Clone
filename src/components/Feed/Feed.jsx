0ef4990015de0e86e01181e944010c892b6bfb91
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-14 02:43:02 -0300

    useEffect de los snapshot y map de los tweets

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,22 @@
+import { useState, useEffect } from 'react'
 import { HiSparkles } from 'react-icons/hi'
+import { onSnapshot, collection, query, orderBy } from '@firebase/firestore'
 
+import { db } from '../../utils/firebase/firebase'
 import InputTweet from '../InputTweet/InputTweet'
+import Tweet from '../Tweet/Tweet'
 
 const Feed = () => {
+  const [tweets, setTweets] = useState([])
+
+  useEffect(
+    () =>
+      onSnapshot(query(collection(db, 'tweets'), orderBy('timestamp', 'desc')), (snapshot) => {
+        setTweets(snapshot.docs)
+      }),
+    [],
+  )
+
   return (
     <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
       <div className="text-[#d9d9d9] flex items-center justify-between py-2 px-3 sticky top-0 z-50 border-b border-gray-700">
@@ -13,6 +27,11 @@ const Feed = () => {
       </div>
 
       <InputTweet />
+      <div className="pb-72">
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} id={tweet.id} tweet={tweet.data()} />
+        ))}
+      </div>
     </div>
   )
 }
