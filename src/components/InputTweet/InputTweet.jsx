71b4c652addce315b5c6c35eadd2a3088c20ce1b
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-14 00:29:21 -0300

    logica de crear el tweet

--- src/components/InputTweet/InputTweet.jsx
+++ src/components/InputTweet/InputTweet.jsx
@@ -7,13 +7,20 @@ import {
   HiOutlineXCircle,
 } from 'react-icons/hi'
 import Picker from '@emoji-mart/react'
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from 'firebase/firestore'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+import { useSession } from 'next-auth/react'
+
+import { db, storage } from '../../utils/firebase/firebase'
 
 /* eslint-disable @next/next/no-img-element */
 const InputTweet = () => {
   const [tweet, setTweet] = useState('')
   const [selectedFile, setSelectedFile] = useState(null)
   const [showEmojis, setShowEmojis] = useState(false)
+  const [loading, setLoading] = useState(false)
   const filePickerRef = useRef(null)
+  const { data: session } = useSession()
 
   const handleChange = ({ target: { value } }) => {
     setTweet(value)
@@ -37,14 +44,58 @@ const InputTweet = () => {
     setTweet(tweet + emoji)
   }
 
-  const addImageToPost = (e) => { }
+  const sendTweet = async () => {
+    if (loading) return
+    setLoading(true)
+
+    const docRef = await addDoc(collection(db, 'tweets'), {
+      id: session.user.uid,
+      username: session.user.name,
+      userImg: session.user.image,
+      tag: session.user.tag,
+      text: tweet,
+      timestamp: serverTimestamp(),
+    })
+
+    const imageRef = ref(storage, `tweets/${docRef.id}/image`)
+
+    if (selectedFile) {
+      await uploadString(imageRef, selectedFile, 'data_url').then(async () => {
+        const downloadURL = await getDownloadURL(imageRef)
+
+        await updateDoc(doc(db, 'tweets', docRef.id), {
+          image: downloadURL,
+        })
+      })
+    }
+
+    setLoading(false)
+    setTweet('')
+    setSelectedFile(null)
+    setShowEmojis(false)
+  }
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader()
+
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0])
+    }
+
+    reader.onload = (readerEvent) => {
+      setSelectedFile(readerEvent.target.result)
+    }
+  }
 
   return (
-    <div className={`border-b border-grey-700 p-3 flex space-x-3 `}>
+    <div
+      className={`border-b border-grey-700 p-3 flex space-x-3 overflow-y-scroll
+       ${loading && 'opacity-60'} `}
+    >
       <img
         alt="User avatar"
         className="h-11 w-11 rounded-full cursor-pointer"
-        src="https://yt3.ggpht.com/yti/AHyvSCA2fez4tq27XJm-NPbYpuzeAOnQvvzAtDPNMXheEw=s88-c-k-c0x00ffffff-no-rj-mo"
+        src={session?.user?.image}
       />
       <div className="w-full divide-y divide-gray-700">
         <div className={`${selectedFile} "pb-7" ${tweet && 'space-x-2.5'}}`}>
@@ -72,40 +123,43 @@ const InputTweet = () => {
             </div>
           )}
         </div>
-        <div className="flex items-center justify-between pt-2.5">
-          <div className="flex items-center ">
-            <div
-              className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out"
-              onClick={() => filePickerRef.current.click()}
-            >
-              <HiOutlinePhotograph className="h-[22px] text-[#1d9bf0]  cursor-pointer" />
-              <input ref={filePickerRef} hidden type="file" onChange={addImageToPost} />
-            </div>
-            <div className="rotate-90 cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out">
-              <HiOutlineChartBar className="h-[22px] text-[#1d9bf0] cursor-pointer" />
-            </div>
-            <div
-              className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out"
-              onClick={handleEmoji}
-            >
-              <HiOutlineEmojiHappy className="h-[22px] text-[#1d9bf0] cursor-pointer" />
-            </div>
-            <div className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out">
-              <HiOutlineCalendar className="h-[22px] text-[#1d9bf0] cursor-pointer" />
-            </div>
-            {showEmojis && (
-              <div className=" absolute mt-[465px] ml-[-40px]">
-                <Picker theme="dark" onEmojiSelect={addEmoji} />
+        {!loading && (
+          <div className="flex items-center justify-between pt-2.5">
+            <div className="flex items-center ">
+              <div
+                className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out"
+                onClick={() => filePickerRef.current.click()}
+              >
+                <HiOutlinePhotograph className="h-[22px] text-[#1d9bf0]  cursor-pointer" />
+                <input ref={filePickerRef} hidden type="file" onChange={addImageToPost} />
               </div>
-            )}
+              <div className="rotate-90 cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out">
+                <HiOutlineChartBar className="h-[22px] text-[#1d9bf0] cursor-pointer" />
+              </div>
+              <div
+                className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out"
+                onClick={handleEmoji}
+              >
+                <HiOutlineEmojiHappy className="h-[22px] text-[#1d9bf0] cursor-pointer" />
+              </div>
+              <div className="cursor-pointer w-9 h-9 hover:bg-[#1d9bf0] hover:bg-opacity-10 flex items-center justify-center rounded-full transition ease-out">
+                <HiOutlineCalendar className="h-[22px] text-[#1d9bf0] cursor-pointer" />
+              </div>
+              {showEmojis && (
+                <div className=" absolute mt-[465px] ml-[-40px]">
+                  <Picker theme="dark" onEmojiSelect={addEmoji} />
+                </div>
+              )}
+            </div>
+            <button
+              className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
+              disabled={!tweet.trim() && !selectedFile}
+              onClick={sendTweet}
+            >
+              tweet
+            </button>
           </div>
-          <button
-            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"
-            disabled={!tweet.trim() && !selectedFile}
-          >
-            tweet
-          </button>
-        </div>
+        )}
       </div>
     </div>
   )
