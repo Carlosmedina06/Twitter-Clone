5d75b783456c7f520e69f2e99668069db32a1162
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-14 00:30:09 -0300

    getserversideprops del provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,34 @@
+import { getProviders, getSession, useSession } from 'next-auth/react'
+
 import Feed from '@/components/Feed/Feed'
 import Sidebar from '@/components/Sidebar/Sidebar'
+import Login from '@/components/Login/Login'
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession()
+
+  if (!session) return <Login providers={providers} />
+
   return (
     <main className="flex min-h-screen max-w-[1500px] mx-auto">
       <Sidebar />
       <Feed />
     </main>
   )
+}
+
+export async function getServerSideProps(context) {
+  // const trendingResults = await fetch('https://jsonkeeper.com/b/NKEV').then((res) => res.json())
+  // const followResults = await fetch('https://jsonkeeper.com/b/WWMJ').then((res) => res.json())
+  const providers = await getProviders()
+  const session = await getSession(context)
+
+  return {
+    props: {
+      // trendingResults,
+      // followResults,
+      providers,
+      session,
+    },
+  }
 }
