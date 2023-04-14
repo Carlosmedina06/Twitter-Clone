04ee3bb942ab920cc1019c60c9ae069b6537fff1
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-14 00:29:38 -0300

    recibo la imagen del usuario

--- src/components/SidebarAccount/SidebarAccount.jsx
+++ src/components/SidebarAccount/SidebarAccount.jsx
@@ -1,17 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
+import { signOut, useSession } from 'next-auth/react'
 import { HiDotsHorizontal } from 'react-icons/hi'
 
 const SidebarAccount = () => {
+  const { data: session } = useSession()
+
   return (
-    <div className="flex items-center justify-center text-[#d9d9d9] xl:-mr-5 xl:ml-auto mt-auto hover:bg-[#d9d9d9] hover:bg-opacity-10 rounded-full cursor-pointer w-[52px] h-[52px] xl:w-auto xl:h-auto xl:py-3 xl:px-4 transition duration-200 ease-out;">
+    <div
+      className="flex items-center justify-center text-[#d9d9d9] xl:-mr-5 xl:ml-auto mt-auto hover:bg-[#d9d9d9] hover:bg-opacity-10 rounded-full cursor-pointer w-[52px] h-[52px] xl:w-auto xl:h-auto xl:py-3 xl:px-4 transition duration-200 ease-out;"
+      onClick={signOut}
+    >
       <img
         alt="User avatar"
         className="h-10 w-10 rounded-full xl:mr-2.5"
-        src="https://yt3.ggpht.com/yti/AHyvSCA2fez4tq27XJm-NPbYpuzeAOnQvvzAtDPNMXheEw=s88-c-k-c0x00ffffff-no-rj-mo"
+        src={session?.user?.image}
       />
       <div className="hidden xl:inline leading-5">
-        <h4 className="font-bold">Carlos Medina</h4>
-        <p className="text-[#6e767d]">@carlomedina06</p>
+        <h4 className="font-bold text-sm">{session?.user?.name}</h4>
+        <p className="text-[#6e767d]">@{session?.user?.tag}</p>
       </div>
       <HiDotsHorizontal className="h-5 hidden xl:inline ml-10" />
     </div>
