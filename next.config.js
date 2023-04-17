09b8e461767d8af37c7a2f9c3589b416dc1bf904
carlosmedina06 <slaawmedina14@gmail.com>
2023-04-17 19:42:44 -0300

    cambio next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['pbs.twimg.com', 'www.jsonkeeper.com', 'rb.gy'],
+  },
 }
 
 module.exports = nextConfig
